8f813285de76eefdc76f6989332bd319fcf2ece7
pyush-18 <piyushjoshi1812@gmail.com>
2025-02-27 12:04:26 +0530

    fixing bugs

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://get-placed-18lp.onrender.com",
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
